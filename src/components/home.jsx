ab013edde5fbf23fd49257c68fc53e6b843e92e4
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-27 22:47:16 -0700

    Adds responsive styling for wedding cakes and bride+groom on home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export class Home extends React.Component {
  
@@ -8,9 +9,23 @@ export class Home extends React.Component {
         <div className="bride-and-groom col-sm-4" />
         <div className="home-content col-sm-8">
         	<h1>Martina and Michael's Wedding Trip 2017</h1>
-        	<div className="wedding-cakes-table">
-            <div className="wedding-cakes" />
-            <div className="table" />
+        	<div className="wedding-cakes-section">
+            <LinkContainer to={{ pathname: 'japan'}}>
+              <div className="wedding-cake japan" >
+                <span className="cake-title">Japan ></span>
+              </div>
+            </LinkContainer>
+            <LinkContainer to={{ pathname: 'taiwan'}}>
+              <div className="wedding-cake taiwan" >
+                <span className="cake-title">Taiwan ></span>
+              </div>
+            </LinkContainer>
+            <LinkContainer to={{ pathname: 'hawaii'}}>
+              <div className="wedding-cake hawaii" >
+                <span className="cake-title">Hawaii ></span>
+              </div>
+            </LinkContainer>
+            <div className="wedding-cake-table" />
           </div>
         </div>
         <div className="bottom-section col-sm-12">
