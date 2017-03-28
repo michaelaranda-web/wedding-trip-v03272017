0cded5ec65a94b0f797643e6471659b548c85d9c
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-28 14:29:27 -0700

    Adds welcome message to home page and adjusts some home page styles

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,20 @@ export class Home extends React.Component {
       <div className="home-page">
         <div className="bride-and-groom col-sm-4" />
         <div className="home-content col-sm-8">
-        	<h1>Martina and Michael's Wedding Trip 2017</h1>
+        	<h1>Martina and Michael's Wedding&nbsp;Trip&nbsp;2017</h1>
+          <div className="welcome-message">
+            <div className="col-sm-6 left-section">
+              Welcome
+              <p>This is some test text I am putting in to see what happens.</p>
+            </div>
+            <div className="col-sm-6 right-section">
+              <ul>
+                <li>Trip Itinerary</li>
+                <li>Our Story</li>
+                <li>Donate to Trip Fund</li>
+              </ul>
+            </div>
+          </div>
         	<div className="wedding-cakes-section">
             <LinkContainer to={{ pathname: 'japan'}}>
               <div className="wedding-cake japan" >
@@ -28,9 +41,7 @@ export class Home extends React.Component {
             <div className="wedding-cake-table" />
           </div>
         </div>
-        <div className="bottom-section col-sm-12">
-          <div className="welcome-message"></div>
-        </div>
+        <div className="bottom-section col-sm-12"></div>
       </div>
     );
   }
