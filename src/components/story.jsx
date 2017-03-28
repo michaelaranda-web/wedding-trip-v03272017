deb75e1a607ac08f6d94cab7caac30699670704d
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-28 16:40:52 -0700

    Add story items to Our Story page

--- src/components/story.jsx
+++ src/components/story.jsx
@@ -6,21 +6,33 @@ export class Story extends React.Component {
         <div className="story-page">
           <h1>Our Story</h1>
           <div className="story-items">
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">
-              CAN YOU SEE ME?
-              <div className="story-item-image"></div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2008"></div>
             </div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
-            <div className="story-item col-md-4 col-sm-6">Test</div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2009"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2010-2011"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2012"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2013"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2014-2015"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2015-2016"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2016"></div>
+            </div>
+            <div className="story-item col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2017"></div>
+            </div>
           </div>
         </div>
     );
