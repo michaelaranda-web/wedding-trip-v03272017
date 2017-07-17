0b9a5e85da592f8f90773ecf2c22c4a5a903bb20
Michael Aranda <michaelaranda0.web@gmail.com>
2017-07-16 22:01:39 -0700

    Add actual stories to story page

--- src/components/story.jsx
+++ src/components/story.jsx
@@ -10,36 +10,117 @@ export class Story extends React.Component {
             </div>
           </div>
 
+          <h4> -> Hover over the images to see each year's story.</h4>
+
           <div className="story-items">
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2008"></div>
+              <div className="story-item-image year-2008">
+                It was a chilly autumn night in a crowded grass field under the stars,
+                where Martina and Michael first met. For one of the last activities of
+                UCSD Warren College's Freshman Orientation, a couple hundred unsuspecting, fledgling Tritons
+                found themselves being asked to participate in a group square dancing session.
+                Two shamelessly enthusiastic kids ended up in the same fateful square,
+                and, in the midst of frenzied fiddling and a flurry of feet, couldn't help but catch each others' attention.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2009"></div>
+              <div className="story-item-image year-2009">
+                Martina and Michael (M&M) spent their first two years together exploring everything UCSD had to offer.
+                Given the massive size of the campus, Martina could often be seen zooming around on her
+                pink beach cruiser named "Jet", and Michael zig-zagging through crowds on his trusty longboard "Aurora".
+                With their chosen steeds, M&M spent every day traversing the campus hills together to get to classes,
+                events, and hangouts. Lucky for them, all the exercise helped keep the infamous "Freshman 15" at bay.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2010-2011"></div>
+              <div className="story-item-image year-2010-2011">
+                Focusing more on classes and exams is a natural part of reaching junior and senior-status in college,
+                but a bit of focus never dulled a true sense of fun and adventure. Since they now knew UCSD like the back of their hands,
+                M&M decided to extend their boundaries a bit - Tzu Ching volunteer events brought them to
+                different campuses like USC and UC Irvine, while dates had them
+                exploring Julian, LA, and Hollywood. A frequent craving for sweets
+                also often led them off-campus to Cheesecake Factory, to buy a slice of (or sometimes a whole...) Godiva Cheescake.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2012"></div>
+              <div className="story-item-image year-2012">
+                Trips to scorching Dallas and breezy Catalina Island made 2012 very special for M&M, but of course,
+                their graduation from UCSD stands out as the defining event of the year.
+                The late-night study sessions fueled by hot cocoa,
+                the early morning shoulder-to-shoulder bus rides to and from campus,
+                the constant switching back and forth in classes between taking notes and browsing Facebook, and
+                the excitement of waiting in line for crêpes made by the friendly French woman at the weekly Farmer's Market
+                would all become memories of an era past.
+                With purple leis draping down the front of their black gowns and a relationship four-years-strong,
+                Martina and Michael bid UCSD farewell at its 2012 graduation ceremony and, together, began their
+                post-college lives.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2013"></div>
+              <div className="story-item-image year-2013">
+                The sky is no limit with M&M. In 2013, a fearless freefaller convinced her boyfriend to join her
+                for a nice, quick drop out of a plane. Although skydiving had never been something Michael imagined
+                attempting before, the sparkle in Martina's eyes and the promise of a memorable birthday experience
+                eventually convinced him to strap on a parachute and take the leap of faith. After about five minutes later
+                of high-altitude hysteria, Michael met with Martina back on Earth
+                and hugged her like only a man with a newfound appreciation for solid ground could.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2014-2015"></div>
+              <div className="story-item-image year-2014-2015">
+                Absence makes the heart grow fonder, or at least Michael and Martina liked to think, as a solo Martina headed north
+                to San Dimas for a year to take on a new job. Suddenly, San Diego had gone from being home sweet home to a weekend
+                getaway spot, which helped Martina realize more than ever just how much she loved America's Finest City.
+                While fending for himself in San Diego, and without his better half to set him straight, some of Michael's
+                habits started to revert to those of a college student -
+                burritos for dinner, sleep schedules revolving around how many episodes of the Walking Dead he had
+                left to watch, and afternoons spent on the sandy shores of La Jolla as a beach bum - being bummed out that Martina wasn't around.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2015"></div>
+              <div className="story-item-image year-2015">
+                Together with their two previous (and practically-family) apartmentmates Michael and My, Martina and Michael
+                (yes, we know that's a lot of M's) moved into a quaint little San Diego townhouse - the first place ever that
+                they could officially call their own! Tall, vibrant plants and palm trees cover the house's front door in shade, which provided
+                much-needed relief from the sun as the newest residents tirelessly carried moving boxes in. With freshly painted walls,
+                newly purchased furniture, and even a small garden in the balcony to tend to, M&M had taken a big step forward into adulthood
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2015-2016"></div>
+              <div className="story-item-image year-2015-2016">
+                "I just got a phone call from...Marvel!" "What?" "They offered me a position!" "What??" "It's...in New York!" "WHAT?!?"
+                When Martina broke the news about the career opportunity to Michael, he, and subsequently both of
+                the couple's families, was shocked by the idea that Martina might spend a year or more making it in Manhattan. But surprise quickly
+                turned to steadfast support, and Martina soon after set off to write a defining chapter of her life. Making the most of the experience,
+                she spent afternoons and weekends sketching all the sights, collected free comic books from work, visited every museum she could,
+                and attended star-studded Marvel movie premieres. After a year of adventures in New York, she hung up the cape and headed back to home base.
+              </div>
             </div>
             <div className="story-item col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2016"></div>
+              <div className="story-item-image year-2016">
+                When Martina returned from New York, she was more than happy to accept an invitation to a Friday dinner and
+                catch-up session with Michael and some friends. It felt like a decade since she had been able
+                to get dressed up, drive in a car, and have a meal with real-deal San Diegans.
+                She soon discovered upon arriving, however, that the San Diego Wine and Culinary Center's
+                "special dinner event" she had been expecting to attend was in reality a private affair meant only for her,
+                and her soon-to-be fiancé. Michael had secretly taken the day off from work to borrow the location's kitchens,
+                prepare a seven-course dinner, and make sure the privately-reserved space was blanketed in candles and music.
+                With steeled nerves and ring at the ready, Michael humbly got down on one knee and asked the inevitable question.
+                She said yes!
+              </div>
             </div>
-            <div className="story-item year-2017 col-md-4 col-sm-6 col-xs-12">
-              <div className="story-item-image year-2017"></div>
+            <div className="story-item bottom-section col-md-4 col-sm-6 col-xs-12">
+              <div className="story-item-image year-2017">
+                On January 23rd (1-2-3!) with their families and two housemates as joyful witnesses,
+                M&M made a trip to a courthouse in San Marcos and finally tied a knot eight years in the making.
+                In place of the traditional wedding ceremony, Martina and Michael decided to take a two-week "wedding tour"
+                to Japan, Taiwan, and Hawaii, during which they were able to visit and celebrate with
+                many friends and family as part of the trip itself. M&M would say they lead a very fulfilling
+                married life together. At the time of writing: they most enjoy spending their days paddling out together
+                to catch friendly waves at both new and favorite surf spots, taking it easy at home, a cafe, or Barnes and
+                Noble and working on their personal projects, and always finding some time to discover new events, restaurants,
+                sights and beaches in their home of San Diego. M&M's wedding marked the end of one adventure; they're now looking forward to the next!
+              </div>
             </div>
           </div>
         </div>
